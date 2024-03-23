d7dce566824aa1b0e4374aeb70f8cb0f40e3fdf6
SarTika116 <sarrtika.01@gmail.com>
2024-03-23 20:21:40 +0700

    Tambahan Skills Section + Edit
    Tambahasan dan perbaikan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 /*=============== SHOW SIDEBAR ===============*/
 
-
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
 
@@ -10,8 +9,28 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab => {
+         tab.addEventListener("click", () => {
+             const target = document.querySelector(tab.dataset.target)
+
+             tabContent.forEach(tabContents => {
+                 tabContents.classList.remove('skills_active')
+             })
+
+             target.classList.add('skills_active')
 
 
+             tabs.forEach(tab => {
+                tab.classList.remove('skills_active')
+            })
+
+            tab.classList.add('skills_active')
+             
+         })
+      })
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
